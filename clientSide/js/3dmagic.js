72d26c95be02dbea63372ad15e74f72dd4df13ac
Andreas Drangel <andreas.drangel@gmail.com>
2014-03-06 17:03:04 +0100

    Now able to click on plane to place out objects.

--- clientSide/js/3dmagic.js
+++ clientSide/js/3dmagic.js
@@ -14,12 +14,14 @@ var FAR = 10000;
 var camera;
 var scene;
 var renderer;
+
+var projector;
 var mesh;
 var controls;
 
 var clock = new THREE.Clock();
 
-var objectArray = new Array();
+var objects = new Array();
 var container;
 
 var map;
@@ -37,6 +39,7 @@ function init() {
 	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT_RATIO, NEAR, FAR);
 	renderer = new THREE.WebGLRenderer();
 	controls = new THREE.FlyControls(camera);
+	projector = new THREE.Projector();
 
 	var light = new THREE.DirectionalLight(0xffffff);
 
@@ -52,6 +55,21 @@ function init() {
 
 
 
+		var PI2 = Math.PI * 2;
+	particleMaterial = new THREE.SpriteCanvasMaterial( {
+
+		color: 0x000000,
+		program: function ( context ) {
+
+			context.beginPath();
+			context.arc( 0, 0, 0.5, 0, PI2, true );
+			context.fill();
+
+		}
+
+	} );
+
+
 	map = createMap(40, 40);
 
 
@@ -59,11 +77,12 @@ function init() {
 
 	// Configure renderer
 	renderer.setSize(width, height);
-//	renderer.setClearColor(0xffffff, 1);
+	renderer.setClearColor(0xffffff, 1);
 	container.appendChild( renderer.domElement );
 
 	render();
 
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 	window.addEventListener( 'resize', onWindowResize, false );
 }
 
@@ -75,8 +94,11 @@ function createMap(width, height) {
 
 	mesh.position.z = -100;
 
+	objects.push(mesh);
 
 	scene.add(mesh);
+
+	return mesh;
 }
 
 function animate() {
@@ -92,9 +114,36 @@ function render() {
 }
 
 function onWindowResize() {
-
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onDocumentMouseDown(event) {
+	event.preventDefault();
+
+	var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+	projector.unprojectVector( vector, camera );
+
+	var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+	var tmp = map;
+	var intersects = raycaster.intersectObjects( objects, true );
+
+	if ( intersects.length > 0 ) {
+		var intersect = intersects[0];
+		var intersectPoint = intersect.point;
+
+		//intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+
+		var material = new THREE.MeshPhongMaterial( { ambient: 0x050505, color: 'red', specular: 0x555555, shininess: 30 } ); 
+		
+		var particle = new THREE.Mesh(new THREE.SphereGeometry(.1, .1, .1), material);
+		particle.position = intersectPoint;
+		particle.scale.x = particle.scale.y = 16;
+		scene.add( particle );
+
+	}
+
 }
